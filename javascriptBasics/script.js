b23ab018eafd0f926966b18e2f0a64e3dcb53be6
ricardojusto <ricardojusto@outlook.com>
2015-12-11 15:39:19 +0000

    update exercises for random number game

---
+++ javascriptBasics/script.js
@@ -0,0 +1,8 @@
+// random number guessing game
+      // var randomNumber = Math.floor(Math.random() * 6) + 1;
+      // var guess = prompt('i am thinking of a number between 1 and 6. what is it?');
+      // if (parseInt(guess)=== randomNumber) {
+      //   document.write('<p>You guessed the number!</p>');
+      // }else{
+      //   document.write("<p>I' m sorry. The number was " + randomNumber + '</p>');
+      // }
